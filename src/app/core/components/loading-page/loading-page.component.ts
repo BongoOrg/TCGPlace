f67f4d231ea4cb0be18db2c846758fc874d6afd6
val <valentin.bonnefon@diiage.org>
2023-06-25 16:05:43 +0200

    ios init

---
+++ src/app/core/components/loading-page/loading-page.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {LoadingController} from "@ionic/angular";
+
+@Component({
+  selector: 'app-loading-page',
+  templateUrl: './loading-page.component.html',
+  styleUrls: ['./loading-page.component.scss'],
+})
+export class LoadingPageComponent {
+  constructor(private loadingCtrl: LoadingController) {}
+
+  async showLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Dismissing after 3 seconds...',
+    });
+
+    await loading.present();
+  }
+
+}
---
+++ src/app/core/services/LoadingService/loading.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {LoadingController} from "@ionic/angular";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingService {
+
+  loading?: HTMLIonLoadingElement;
+
+  constructor(public loadingController: LoadingController) { }
+
+  async show() {
+    this.loading = await this.loadingController.create({
+      message: 'Please wait...',
+      spinner: 'crescent',  // Changer le type de spinner selon vos besoins.
+      duration: 2000
+    });
+    await this.loading.present();
+  }
+
+  async hide() {
+    if(this.loading != null){
+      await this.loading.dismiss();
+    }
+  }
+}
