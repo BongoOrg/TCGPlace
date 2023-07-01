54a6c73ae4ec5074e9613a15fc2da65cc2729d48
Mickael <mickael.jacquet@diiage.org>
2023-07-01 18:38:02 +0200

    Ajout des fichiers RGPD sur le site avec correctif des export/declaration dans le module RGPD

--- src/app/rgpd/rgpd.module.ts
+++ src/app/rgpd/rgpd.module.ts
@@ -8,14 +8,29 @@ import { RgpdPageRoutingModule } from './rgpd-routing.module';
 
 import { RgpdPage } from './rgpd.page';
 import { RouterModule } from '@angular/router';
+import { PolitiqueDeConfidentialiteComponent } from './politique-de-confidentialite/politique-de-confidentialite.component';
+import { ReglesDuCatalogueComponent } from './regles-du-catalogue/regles-du-catalogue.component';
+import { PolitiqueDeCookiesComponent } from './politique-de-cookies/politique-de-cookies.component';
+import { TermesEtCGUComponent } from './termes-et-cgu/termes-et-cgu.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    IonicModule,
+    IonicModule.forRoot(),
     RgpdPageRoutingModule
   ],
-  declarations: [RgpdPage]
+  exports: [
+    PolitiqueDeConfidentialiteComponent,
+    ReglesDuCatalogueComponent,
+    PolitiqueDeCookiesComponent,
+    TermesEtCGUComponent
+  ],
+  declarations: [
+    RgpdPage,
+    PolitiqueDeConfidentialiteComponent,
+    ReglesDuCatalogueComponent,
+    PolitiqueDeCookiesComponent,
+    TermesEtCGUComponent]
 })
 export class RgpdPageModule {}
