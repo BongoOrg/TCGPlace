9bc2475370e0689377bd969a5db62a48df0b3355
Paranctiss <fli1@live.fr>
2024-01-08 16:08:11 +0100

    set dev env

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
-export const CATALOG_URL = 'http://13.95.7.2:5195';
-export const POST_URL = 'http://13.95.7.2:5079';
-export const AUTH_URL = 'http://13.95.7.2:5161';
-export const INVOICE_URL = 'http://13.95.7.2:5191';
-export const MESSAGERIE_URL = 'http://13.95.7.2:5177';
+export const CATALOG_URL = 'http://51.210.180.174:5195';
+export const POST_URL = 'http://51.210.180.174:5079';
+export const AUTH_URL = 'http://51.210.180.174:5161';
+export const INVOICE_URL = 'http://51.210.180.174:5191';
+export const MESSAGERIE_URL = 'http://51.210.180.174:5177';
